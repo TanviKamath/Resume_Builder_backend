f531b78305386ca12239befee7e7bb816514900c
Tanvi Kamath <tanvikamath22@gmail.com>
2025-11-23 13:30:15 +0530

    nav

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,39 @@
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Play, Star } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Hero = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <section className="relative min-h-screen bg-gradient-hero pt-32 pb-20">
+    <section className="relative min-h-screen bg-gradient-hero pt-16 pb-20">
+      <nav className="absolute top-0 left-0 w-full z-50 flex items-center justify-between py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
+        <a href="https://prebuiltui.com">
+          <img src="/logo.svg" alt="Logo" />
+        </a>
+
+        <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
+          <a href="#" className="hover:text-indigo-600 transition">Home</a>
+          <a href="#features" className="hover:text-indigo-600 transition">Features</a>
+          <a href="#testimonials" className="hover:text-indigo-600 transition">Testimonials</a>
+          <a href="#cta" className="hover:text-indigo-600 transition">Contact</a>
+        </div>
+
+        <div className="flex gap-2">
+          <a href="#" className="hidden md:block px-6 py-2 bg-indigo-500 hover:bg-indigo-700 active:scale-95 transition-all rounded-full text-white">
+            Get started
+          </a>
+          <a href="#" className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
+            Login
+          </a>
+        </div>
+
+        <button onClick={() => setMenuOpen(true)} className="md:hidden active:scale-90 transition" >
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" stroke="currentColor" strokeWidth="2" className="lucide lucide-menu" >
+            <path d="M4 5h16M4 12h16M4 19h16" />
+          </svg>
+        </button>
+      </nav>
       <div className="container mx-auto px-6">
         <div className="max-w-5xl mx-auto text-center">
           {/* Social Proof */}
